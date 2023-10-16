cb2dcc905e2b1b226d8c38205f971d5075faba15
goodliu <liuzengh@qq.com>
2023-10-16 11:43:28 +0800

    all: update copyright headers

--- internal/locker/locker.go
+++ internal/locker/locker.go
@@ -1,8 +1,8 @@
-// Tencent is pleased to support the open source community by making tRPC available.
+// Tencent is pleased to support the open source community by making tnet available.
 // Copyright (C) 2023 THL A29 Limited, a Tencent company. All rights reserved.
 // If you have downloaded a copy of the tRPC source code from Tencent,
-// please note that tRPC source code is licensed under the Apache 2.0 License,
-// A copy of the Apache 2.0 License is included in this file.
+// please note that tnet source code is licensed under the Apache 2.0 License,
+// A copy of the Apache 2.0 License can be found in the LICENSE file.
 
 // Package locker provides locking utilities.
 package locker
