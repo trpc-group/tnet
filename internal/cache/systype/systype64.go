c2fc68cde221da8aa606126cc547977f6f9d40cd
goodliu <liuzengh@qq.com>
2023-10-16 16:25:07 +0800

    all: update copyright headers
    to keep consistent with the description in the CONTRIBUTING.md file of the trpc repository
    
    https://github.com/trpc-group/trpc/blob/main/CONTRIBUTING.md#copyright-headers

--- internal/cache/systype/systype64.go
+++ internal/cache/systype/systype64.go
@@ -1,8 +1,15 @@
-// Tencent is pleased to support the open source community by making tnet available.
-// Copyright (C) 2023 THL A29 Limited, a Tencent company. All rights reserved.
+//
+//
+// Tencent is pleased to support the open source community by making tRPC available.
+//
+// Copyright (C) 2023 THL A29 Limited, a Tencent company.
+// All rights reserved.
+//
 // If you have downloaded a copy of the tRPC source code from Tencent,
-// please note that tnet source code is licensed under the Apache 2.0 License,
-// A copy of the Apache 2.0 License can be found in the LICENSE file.
+// please note that tRPC source code is licensed under the  Apache 2.0 License,
+// A copy of the Apache 2.0 License is included in this file.
+//
+//
 
 //go:build amd64 || arm64 || mips64 || mips64le || ppc64 || ppc64le || riscv64 || s390x || loong64
 // +build amd64 arm64 mips64 mips64le ppc64 ppc64le riscv64 s390x loong64
